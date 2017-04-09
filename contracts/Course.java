3bff5b323ea90ebacaae545d995965d078d350c5
ISpectruM <ispectrumcode@gmail.com>
2017-04-09 17:52:33 +0300

    Unit Testing added

--- contracts/Course.java
+++
@@ -1,6 +0,0 @@
-package bg.softuni.contracts;
-
-import bg.softuni.models.SoftUniCourse;
-
-public interface Course extends Comparable<SoftUniCourse> {
-}
--- contracts/Student.java
+++
@@ -1,6 +0,0 @@
-package bg.softuni.contracts;
-
-import bg.softuni.models.SoftUniStudent;
-
-public interface Student extends Comparable<SoftUniStudent>{
-}
